f2773f925ae74cbce23f4e86c71c93ef7fc85b1a
paramjeet.singh <paramjeet.singh@exp-inc.com>
2019-08-26 14:34:35 +0530

    Owned.sol

---
+++ contracts/Owned.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.5.1;
+
+// ----------------------------------------------------------------------------
+// Owned contract
+// ----------------------------------------------------------------------------
+contract Owned {
+    address public owner;
+    address public newOwner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner,'sender should be owner');
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+    function acceptOwnership() public {
+        require(msg.sender == newOwner,'sender should be new owner');
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+}
